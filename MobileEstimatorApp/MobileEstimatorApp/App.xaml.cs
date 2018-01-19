7dd3eaed1c9bd596c4564ee5c26522b05f3b5cc8
Rabbit <venkatasivaprasadreddypulagam@gmail.com>
2018-01-19 11:32:22 +0530

    Committing changes to project

--- MobileEstimatorApp/MobileEstimatorApp/App.xaml.cs
+++ MobileEstimatorApp/MobileEstimatorApp/App.xaml.cs
@@ -2,28 +2,28 @@
 
 namespace MobileEstimatorApp
 {
-	public partial class App : Application
-	{
-		public App()
-		{
-			InitializeComponent();
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
 
-			MainPage = new MobileEstimatorAppPage();
-		}
+            MainPage = new VehicleInfoPageOne();
+        }
 
-		protected override void OnStart()
-		{
-			// Handle when your app starts
-		}
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
 
-		protected override void OnSleep()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnResume()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
